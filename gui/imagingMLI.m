d228e9142a79d95ba79320a54414689dab05bd72
csvetter <63097190+claravetter@users.noreply.github.com>
2022-07-27 16:55:33 +0200

    imaging mli viewer

---
+++ gui/imagingMLI.m
@@ -0,0 +1,14 @@
+function imagingMLI(casenum, MLIcont)
+%global stMLI
+
+y = MLIcont.Y_mapped(casenum,:)';
+%y_ciu = MLIcont.Y_mapped_ciu(casenum,:);
+%y_cil = MLIcont.Y_mapped_cil(casenum,:);
+
+brainmask = evalin('base', 'NM.brainmask{1}'); % curModal 
+
+nk_WriteVol(y,'temp', 2, brainmask, [], 0, 'gt');
+mli_orthviews('Image','temp.nii'); 
+
+
+end
