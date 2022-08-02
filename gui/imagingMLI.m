675adb5874ca77471a3bd5edddf767b462bceb8c
csvetter <63097190+claravetter@users.noreply.github.com>
2022-08-02 14:16:32 +0200

    MLI result viewer

--- gui/imagingMLI.m
+++ gui/imagingMLI.m
@@ -1,5 +1,5 @@
 function imagingMLI(casenum, MLIcont, APPstMLI)
-%global stMLI
+
 global stMLI
 stMLI = APPstMLI;
 y = MLIcont.Y_mapped(casenum,:)';
@@ -10,6 +10,17 @@ brainmask = evalin('base', 'NM.brainmask{1}'); % curModal
 
 nk_WriteVol(y,'temp', 2, brainmask, [], 0, 'gt');
 mli_orthviews('Image','temp.nii'); 
-
+colormap(stMLI.NMaxes(3), jet);
+colormap(stMLI.NMaxes(2), jet);
+colormap(stMLI.NMaxes(1), jet);
+pos = stMLI.NMaxes(3).Position;
+cl = colorbar(stMLI.NMaxes(3));
+cl.TickLabels=linspace(min(y),max(y),6);
+%cl.Label.String = meas{measind};
+cl.Label.FontWeight = 'bold';
+cl.Label.FontSize = 11;
+stMLI.NMaxes(3).Position = pos;
+% handles.axes33.XLabel.String='';
+% handles.axes33.YLabel.String='';
 
 end
