0274e4ce828627653cbdd05ef85205b896635fc2
csvetter <63097190+claravetter@users.noreply.github.com>
2022-09-02 09:21:58 +0200

    NEW: gradient boosting regressor & changed defaults of gradient boosting classifier

--- Python/py_classGRDBST_predict.py
+++ Python/py_classGRDBST_predict.py
@@ -1,3 +1,20 @@
+'''
+Author: Clara Vetter 
+Last changed: 02.09.2022
+
+This script is called by nk_GetTestPerf_GRDBST.m and computes the test 
+performance of a gradient boosting classification model on the test sample. 
+
+Input from MATLAB: 
+    - model_name = path to file containing the model's information from 
+    training
+    - test_feat = the test set (test X)
+
+Output: 
+    - the predictions are stored in a MATLAB file GBpredict_output.mat in 
+    the analysis' folder (which will be loaded in by Matlab to proceed) 
+'''
+
 from sklearn.ensemble import GradientBoostingClassifier
 import pickle
 from scipy.io import savemat
--- Python/py_classGRDBST_train.py
+++ Python/py_classGRDBST_train.py
@@ -1,5 +1,28 @@
-# train GRDBST Classifier
-# Python script to train a gradient boosting classifier from NeuroMiner
+'''
+Author: Clara Vetter 
+Last changed: 02.09.2022
+
+This script is called by nk_GetParam2_GRDBST.m and trains a gradient 
+boosting classification model on the training data. 
+
+Input from MATLAB: 
+    - parameters: 
+        n_est
+        l
+        lr
+        subsamp
+        n_maxdepth
+    - feat = training data
+    - lab = label 
+    - rootdir = path to analysis directory
+
+Output: 
+    - the model is stored in the analysis directory in a .sav file that 
+    will later be read in by the corresponding test script 
+    (py_classGRDBST_predict.py). The file name is unique.
+    
+'''
+
 
 from sklearn.ensemble import GradientBoostingClassifier
 import pickle
