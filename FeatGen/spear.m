2fd56fd74acf73d9afa8fd166dbbb98454d1ac36
neurominer-git <57187268+neurominer-git@users.noreply.github.com>
2023-02-07 15:41:59 +0100

    Feb 23 update

--- FeatGen/spear.m
+++ FeatGen/spear.m
@@ -1,4 +1,4 @@
-function r = spear(x,y)
+function [r, p, Td] = spear(x,y)
 
 % Find the data length
 N = length(x);
@@ -12,6 +12,15 @@ S = crank(y)';
 % Calculate the Spearman correlation coefficient
 r = 1-6*sum((R-S).^2)/N/(N^2-1);
 
+% Calculate the t statistic
+if r == 1 || r == -1
+    t = r*inf;
+else
+    t=r.*sqrt((N-2)./(1-r.^2));
+end
+% Calculate the p-values
+p=2*(1-tcdf(abs(t),N-2));
+
 function r=crank(x)
 
 u = unique(x); lU = length(u);
@@ -20,11 +29,10 @@ u = unique(x); lU = length(u);
 r = (1:length(x))';
 r = r(z2);
 
-for i=1:lU;
+for i=1:lU
     s = u(i) == x;
     r(s,1) = mean(r(s));
 end
-
 
 
 
