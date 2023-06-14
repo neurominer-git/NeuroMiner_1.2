f8ac4356f4cc1b80643380d4e52352e95b9fbc63
neurominer-git <57187268+neurominer-git@users.noreply.github.com>
2023-06-14 17:49:22 +0200

    Update nk_VisXWeight.m

--- nk_VisXWeight.m
+++ nk_VisXWeight.m
@@ -223,11 +223,7 @@ for n=1:nM
                         pIND = naPX.(IND);
                     end
                     % Eliminated features have to be re-introduced
-                    try
-                        tmW = zeros(length(pIND),1); tmW(pIND) = nmW; nmW = tmW;   
-                    catch
-                        fprintf('problem');
-                    end
+                    tmW = zeros(length(pIND),1); tmW(pIND) = nmW; nmW = tmW;   
                     tmP = false(length(pIND),1); tmP(pIND) = nmP; nmP = tmP;
                     if ~isempty(PA)
                          tmPA = tmW; tmPA(naPX.(IND)) = nmPA; nmPA = tmPA; 
